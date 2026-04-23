e5e6e72a88d32b40f2640b5e2882f039cae437f0
growpratham@gmail.com <cglwkey@gmail.com>
2026-04-23 23:36:32 +0530

    Add: Largest element in array

---
+++ 01-Arrays/Easy/Largest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Largest{
+
+  public static int largestElement(int[] arr, int n){
+    int largest = arr[0];
+    for (int i = 0; i < n ; i++) {
+      if(arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in);) {
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      for (int i = 0; i < n; i++) {
+          arr[i] = sc.nextInt();
+      } 
+
+      System.out.println("Largest Element: " + largestElement(arr, n));
+    }
+  }
+}
