4c4aaa93b2c87f8e06fc7199d0633d630e22f633
grow-pratham <growpratham@gmail.com>
2026-04-27 08:06:30 +0530

    Add: Majority element occured in an array

--- 01-Arrays/Medium/MajorityElement.java
+++ 01-Arrays/Medium/MajorityElement.java
@@ -1,3 +1,24 @@
+// Problem Statement: Given an integer array nums of size n, return the majority element of the array.
+
+// The majority element of an array is an element that appears more than n/2 times in the array. The array is guaranteed to have a majority element.
+
+// Examples
+// Example 1:
+// Input:
+//  nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]  
+// Output:
+//  7  
+// Explanation:
+//  The number 7 appears 5 times in the 9-sized array, making it the most frequent element.
+
+// Example 2:
+// Input:
+//  nums = [1, 1, 1, 2, 1, 2]  
+// Output:
+//  1  
+// Explanation:
+//  The number 1 appears 4 times in the 6-sized array, making it the most frequent element.
+
 import java.util.*;
 
 class Solution{
